a20f0480c26e379440147147b34f33d73072309e
Siti Laelatur Rochmah <lelaela6@gmail.com>
2021-07-28 22:26:19 +0700

    Add query parameter on category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -22,9 +22,16 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return CategoryResource::collection($this->categoryRepository->all());
+        if ($request->filled('type')) {
+            $category = $this->categoryRepository->findWhere([
+                'type' => $request->type
+            ]);
+        } else {
+            $category = $this->categoryRepository->all();
+        }
+        return CategoryResource::collection($category);
     }
 
     /**
